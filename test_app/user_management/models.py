1d4d8b245b6cf631b415fcbd17de2889e95af27c
chedi toueiti <chedi.toueiti@gmail.com>
2015-06-11 10:27:46 +0100

    this one is so big of a failure. No comment, wrong copy/paste

--- test_app/user_management/models.py
+++ test_app/user_management/models.py
@@ -1,14 +1,13 @@
 from django.db                          import models
 from django.utils.translation           import ugettext_lazy as _
-from localflavor.generic.forms          import IBANFormField
 from django.contrib.auth.models         import User
+from localflavor.generic.models         import IBANField
 from localflavor.generic.countries.sepa import IBAN_SEPA_COUNTRIES
-
 
 class UserInformation(models.Model):
     last_name    = models.CharField(_('last name'   ), max_length=20                        )
     first_name   = models.CharField(_('first name'  ), max_length=20                        )
-    IBAN_account = IBANFormField   (_('IBAN account'), include_countries=IBAN_SEPA_COUNTRIES)
+    IBAN_account = IBANField       (_('IBAN account'), include_countries=IBAN_SEPA_COUNTRIES)
 
     account_manager = models.ForeignKey(User, verbose_name=_('account manager'))
 

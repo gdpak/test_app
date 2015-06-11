cef703e2ffc54faa7374ba1be75f8a30f48c94a9
chedi toueiti <chedi.toueiti@gmail.com>
2015-06-11 08:24:05 +0100

    abondoning suite in the end :-(

--- test_app/test_app/settings.py
+++ test_app/test_app/settings.py
@@ -1,7 +1,7 @@
-from os.path                  import join
-from os.path                  import abspath
-from os.path                  import dirname
-from django.utils.translation import ugettext_lazy as _
+from os.path        import join
+from os.path        import abspath
+from os.path        import dirname
+from django.contrib import messages
 
 
 DEBUG                 = True
@@ -18,7 +18,7 @@ def getpath(resource_path):
     return abspath(join(BASE_DIR, resource_path))
 
 INSTALLED_APPS = (
-    'suit',
+    'django_admin_bootstrapped',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -53,7 +53,6 @@ TEMPLATES = [{
         'context_processors': [
             'django.core.context_processors.request',
             'django.template.context_processors.debug',
-            'django.template.context_processors.request',
             'django.contrib.auth.context_processors.auth',
             'django.contrib.messages.context_processors.messages',
 ]}}]
@@ -113,25 +112,11 @@ LOGGING = {
         'django.request': {'level': 'ERROR', 'handlers' : ['mail_admins'           ], 'propagate': False},
 }}
 
-SUIT_CONFIG = {
-    'VERSION'                : "0.1",
-    'ADMIN_NAME'             : _('Test App'),
-    'SEARCH_URL'             : '/admin/user_management/user_information',
-    'LIST_PER_PAGE'          : 30,
-    'HEADER_DATE_FORMAT'     : 'l, j. F Y',
-    'HEADER_TIME_FORMAT'     : 'H:i',
-    'MENU_OPEN_FIRST_CHILD'  : True,
-    'SHOW_REQUIRED_ASTERISK' : True,
-    'CONFIRM_UNSAVED_CHANGES': True,
-    'MENU': (
-        '-',
-        {'label': _('Operations'), 'icon': 'icon-briefcase' , 'models': ('artemis.partner', 'artemis.activity', 'artemis.activityobserveridentity'                    )},
-        {'label': _('Forms'     ), 'icon': 'icon-tasks'     , 'models': ('artemis.form', 'artemis.formtype', 'artemis.formgroup', 'artemis.formfield'                 )},
-        {'label': _('Results'   ), 'icon': 'icon-inbox'     , 'models': ('artemis.formfieldresult', 'artemis.formgroupresult', 'artemis.formresult'                   )},
-        {'label': _('Observers' ), 'icon': 'icon-eye-open'  , 'models': ('artemis.observer', 'artemis.observerrole'                                                   )},
-        {'label': _('Locations' ), 'icon': 'icon-map-marker', 'models': ('artemis.location', 'artemis.locationtype', 'artemis.pollingstation', 'artemis.pollingcenter')},
-        {'label': _('Contacts'  ), 'icon': 'icon-bullhorn'  , 'models': ('artemis.phone', 'artemis.email' , 'artemis.address'                                         )},
-        '-',
-        {'label': _('Documentation'), 'icon': 'icon-question-sign', 'url': '/static/documentation/index.html'}
-    ),
+
+DAB_FIELD_RENDERER = 'django_admin_bootstrapped.renderers.BootstrapFieldRenderer'
+
+MESSAGE_TAGS = {
+            messages.SUCCESS: 'alert-success success',
+            messages.WARNING: 'alert-warning warning',
+            messages.ERROR: 'alert-danger error'
 }

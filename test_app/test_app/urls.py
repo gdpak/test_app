19857deed8acbdd5079c938ee23ecda3b9ede00e
chedi toueiti <chedi.toueiti@gmail.com>
2015-06-11 12:43:25 +0100

    adding an index view

--- test_app/test_app/urls.py
+++ test_app/test_app/urls.py
@@ -2,6 +2,10 @@ from django.contrib   import admin
 from django.conf.urls import url
 from django.conf.urls import include
 
+from user_management.views import index
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$'     , index, name='index'),
+    url(''        , include('social.apps.django_app.urls', namespace='social')),
 ]
--- test_app/user_management/views.py
+++ test_app/user_management/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts        import render_to_response
+from django.template.context import RequestContext
 
-# Create your views here.
+
+def index(request):
+   return render_to_response(
+       'index.html',
+       context_instance=RequestContext(
+           request,
+           {'user': request.user}))

8b65cc3cea42eddb3fb2acdcd8cdd8eb8224d75b
chedi toueiti <chedi.toueiti@gmail.com>
2015-06-13 04:39:27 +0100

    adding logout url

--- test_app/test_app/urls.py
+++ test_app/test_app/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls import url
 from django.conf.urls import include
 
 from user_management.views import index
+from user_management.views import logout
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$'     , index, name='index'),
-    url(''        , include('social.apps.django_app.urls', namespace='social')),
+    url(r'^admin/'  , include(admin.site.urls)),
+    url(r'^$'       , index),
+    url(''          , include('social.apps.django_app.urls', namespace='social')),
+    url(r'^logout/$', logout),
 ]

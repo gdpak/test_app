c4c4594a82f271baac1e24549f4f5bdb7d9bc8d9
chedi toueiti <chedi.toueiti@gmail.com>
2015-06-16 06:08:58 +0100

    adding the watchdog package

--- virtualenv_dependencies.sh
+++ virtualenv_dependencies.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-dnf install -y gcc libpng-devel libtiff-devel libxml2-devel openjpeg2-devel postgresql-devel
+dnf install -y gcc libpng-devel libtiff-devel libxml2-devel openjpeg2-devel postgresql-devel \
+    libyaml-devel

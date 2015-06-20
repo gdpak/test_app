bb3d29f08c6028a2a866e999d8a96f6d957d4747
chedi toueiti <chedi.toueiti@gmail.com>
2015-06-20 13:31:30 +0100

    adding locking to the gunicorn reloading to avoid the initial avalanche of updates

--- restart_gunicorn.sh
+++ restart_gunicorn.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-gunicorn_status=$(supervisorctl status  gunicorn_workers | awk '{print($2)}')
+(
+    flock -n 200
+    gunicorn_status=$(supervisorctl status  gunicorn_workers | awk '{print($2)}')
 
-if [ $gunicorn_status == 'RUNNING' ]; then
-    supervisorctl restart gunicorn_workers
-elif [ $gunicorn_status != 'RESTARTING']; then
-    supervisorctl start gunicorn_workers
-fi
+    if [ $gunicorn_status == 'RUNNING' ]; then
+        supervisorctl restart gunicorn_workers
+    elif [ $gunicorn_status != 'RESTARTING']; then
+        supervisorctl start gunicorn_workers
+    fi
+) 200>.gunicorn_reload.lock

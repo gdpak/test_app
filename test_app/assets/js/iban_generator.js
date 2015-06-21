d0579a946372bff3ca8401d154f26825bdeb228b
chedi toueiti <chedi.toueiti@gmail.com>
2015-06-21 15:05:29 +0000

    initial generator web service action

--- test_app/assets/js/iban_generator.js
+++ test_app/assets/js/iban_generator.js
@@ -1,15 +1,34 @@
 $(function(){
+    $('.random_option'           ).prop('disabled', true).prop('checked' , true);
+    $('#generator_country'       ).prop('disabled', true);
+    $('.randomizable_input'      ).prop('disabled', true);
+    $('#generator_random_country').prop('checked' , true);
+
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            function getCookie(name) {
+                var cookieValue = null;
+                if (document.cookie && document.cookie != '') {
+                    var cookies = document.cookie.split(';');
+                    for (var i = 0; i < cookies.length; i++) {
+                        var cookie = jQuery.trim(cookies[i]);
+                        if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                            break;
+                }}}
+                return cookieValue;
+            }
+
+            if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+                xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+    }}});
+
     update_input_masks = function() {
         selected = $("#generator_country :selected");
 
         $("#generator_bank"   ).inputmask({mask: selected.attr("bank_mask"   )});
         $("#generator_account").inputmask({mask: selected.attr("account_mask")});
     }
-
-    $('.random_option'           ).prop('disabled', true).prop('checked' , true);
-    $('#generator_country'       ).prop('disabled', true);
-    $('.randomizable_input'      ).prop('disabled', true);
-    $('#generator_random_country').prop('checked' , true);
 
     $('#generator_random_country').click(function(){
         lock_status = $(this).is(':checked');
@@ -31,4 +50,15 @@ $(function(){
             .find('.randomizable_input')
             .prop('disabled', $(this).is(':checked'));
     });
-})
+
+    $('#generator_submit').click(function(event){
+        $.ajax({
+            url    : generator_url,
+            data   : $('#generator_form').serializeArray(),
+            method : "POST",
+            success: function(data){
+                console.log(data);
+            }
+        });
+    });
+});

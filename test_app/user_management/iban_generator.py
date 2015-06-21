ee521aa1c82f9c04126dd206137ac736596e2071
chedi toueiti <chedi.toueiti@gmail.com>
2015-06-21 18:05:32 +0100

    finalization of the iban generator module

--- test_app/user_management/iban_generator.py
+++ test_app/user_management/iban_generator.py
@@ -1,14 +1,12 @@
 from random                   import choice
 from string                   import digits
 from string                   import ascii_uppercase
-from collections              import namedtuple
 from django.utils.translation import ugettext_lazy as _
 
 from user_management.iban_specification import IBAN_SPCIFICATION_CONFIG
 
-mixed            = ascii_uppercase + digits
-letters          = ascii_uppercase
-GenerationResult = namedtuple('GenerationResult', ['status', 'message', 'result'])
+mixed   = ascii_uppercase + digits
+letters = ascii_uppercase
 
 
 class IBANGenerator(object):
@@ -18,8 +16,9 @@ class IBANGenerator(object):
         'a' : lambda length: u''.join(choice(letters) for _ in range(length)),
     }
 
-    def checksum(self, value):
-        value = value[4:] + value[:2] + '00'
+    def checksum(self, country_code, bank, account):
+        value = bank + account + country_code + '00'
+        value = value.upper()
         value_digits = ''
         for x in value:
             if '0' <= x <= '9':
@@ -28,13 +27,11 @@ class IBANGenerator(object):
                 value_digits += str(ord(x) - 55)
             else:
                 raise Exception('{} is not a valid character for IBAN.'.format(x))
-            return '%02d' % (98 - int(value_digits) % 97)
+        return '%02d' % (98 - int(value_digits) % 97)
 
-    def calc_iban(self, country, bank, account, alternative=0):
+    def calc_iban(self, country, bank, account):
         account  = account.zfill(country.account_field_length)
-        checksum = self.checksum(country.country_code + bank.upper() + account.upper())
-        if alternative:
-            checksum = str(int(checksum) % 97).zfill(2)
+        checksum = self.checksum(country.country_code, bank, account)
         return country.country_code + checksum + bank + account
 
     def randomize_field(self, field_specification):
@@ -44,9 +41,11 @@ class IBANGenerator(object):
 
     def generate(self, country_code=None, bank=None, account=None):
         result = {
-            'bank'        : bank,
-            'account'     : account,
-            'country_code': country_code,
+            'bank'             : bank,
+            'status'           : True,
+            'account'          : account,
+            'country_code'     : country_code,
+            'generator_message': _('Generation successful')
         }
 
         if country_code is None:
@@ -54,14 +53,19 @@ class IBANGenerator(object):
         elif country_code in IBAN_SPCIFICATION_CONFIG.keys():
             country = IBAN_SPCIFICATION_CONFIG[country_code]
         else:
-            return GenerationResult(False, _('Invalid country code: {}').format(country), result)
+            result.update({
+                'status'           : False,
+                'generator_message': _('Invalid country code: {}').format (country)})
+            return result
 
         result['bank'          ] = bank    if bank    else self.randomize_field(country.bank_specification   )
         result['account'       ] = account if account else self.randomize_field(country.account_specification)
         result['country_code'  ] = country.country_code
-        result['generated_iban'] = self.calc_iban(country, result['bank'], result['account'])
 
         try:
-            return GenerationResult(True, _('Generation successful'), result)
+            result['generated_iban'] = self.calc_iban(country, result['bank'], result['account'])
         except Exception as e:
-            return GenerationResult(False, e.message, result)
+            result.update({
+                'status'           : False,
+                'generator_message': e.message})
+        return result

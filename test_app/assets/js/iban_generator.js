6e3ef0b5717a77df1a3f912dfa70c4a50993ff8c
chedi toueiti <chedi.toueiti@gmail.com>
2015-06-22 12:28:02 +0100

    moving the ajax scrf configuration in a separate file and loading it on in the root template

--- test_app/assets/js/iban_generator.js
+++ test_app/assets/js/iban_generator.js
@@ -1,5 +1,4 @@
 $(function(){
-
     $('#generator_id').on('shown.bs.modal', function () {
         $("#generator_bank"   ).val('');
         $("#generator_account").val('');
@@ -10,24 +9,6 @@ $(function(){
         $('#generator_random_country').prop('checked' , true);
     });
 
-    $.ajaxSetup({
-        beforeSend: function(xhr, settings) {
-            function getCookie(name) {
-                var cookieValue = null;
-                if (document.cookie && document.cookie != '') {
-                    var cookies = document.cookie.split(';');
-                    for (var i = 0; i < cookies.length; i++) {
-                        var cookie = jQuery.trim(cookies[i]);
-                        if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                            break;
-                }}}
-                return cookieValue;
-            }
-
-            if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-                xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-    }}});
 
     update_input_masks = function() {
         selected = $("#generator_country :selected");

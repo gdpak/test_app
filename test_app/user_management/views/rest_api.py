6a43154044a54bd49454f6285e37716269bc4b83
chedi toueiti <chedi.toueiti@gmail.com>
2015-06-22 12:31:49 +0100

    implementing the account deletion via rest framework

--- test_app/user_management/views/rest_api.py
+++ test_app/user_management/views/rest_api.py
@@ -1,16 +1,14 @@
+from json                           import loads
 from rest_framework.views           import APIView
 from rest_framework.response        import Response
 from django.utils.decorators        import method_decorator
 from django.contrib.auth.decorators import login_required
 
+from user_management.models         import UserInformation
 from user_management.iban_generator import IBANGenerator
 
 
-class IBANGeneratorView(APIView):
-    @method_decorator(login_required)
-    def get(self, request, format=None):
-        return Response({})
-
+class IBANGeneratorApiView(APIView):
     @method_decorator(login_required)
     def post(self, request, format=None):
         generator = IBANGenerator()
@@ -19,3 +17,14 @@ class IBANGeneratorView(APIView):
             bank         = request.POST.get('generator_bank'   ),
             account      = request.POST.get('generator_account'),
             country_code = request.POST.get('generator_country')))
+
+
+class DeleteAccountsApiView(APIView):
+    @method_decorator(login_required)
+    def post(self, request, format=None):
+        try:
+            accounts = loads(request.POST['accounts'])
+            UserInformation.objects.filter(id__in=accounts).delete()
+            return Response({'status': True, 'message': ''})
+        except Exception as e:
+            return Response({'status': False, 'message': str(e)})

559944befe73ca5e6206b9b3f4a03b82164fb532
chedi toueiti <chedi.toueiti@gmail.com>
2015-06-24 17:51:49 +0100

    typo error

--- virtualenv_dependencies.sh
+++ virtualenv_dependencies.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-dnf install -y gcc libxml2-devel postgresql-devel libyaml-devel \
-    python3-pillow-devel python-pillow-devel postgreql-plpython \
+dnf install -y gcc libxml2-devel postgresql-devel libyaml-devel  \
+    python3-pillow-devel python-pillow-devel postgresql-plpython \
     postgresql-plpython3

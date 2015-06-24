fa5d05f39ac6e1a388fe64813870694fadf76bd7
chedi toueiti <chedi.toueiti@gmail.com>
2015-06-24 18:38:11 +0100

    adding wheel format support

--- virtualenv_dependencies.sh
+++ virtualenv_dependencies.sh
@@ -2,4 +2,4 @@
 
 dnf install -y gcc libxml2-devel postgresql-devel libyaml-devel  \
     python3-pillow-devel python-pillow-devel postgresql-plpython \
-    postgresql-plpython3
+    postgresql-plpython3 python3-wheel python-wheel
